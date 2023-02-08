e55643bb5e020b5a7c6fb41fce01982c51c47eb1
Akhilesh-Makam <120730075+Akhilesh-Makam@users.noreply.github.com>
2023-02-08 17:15:37 -0500

    Update main.go
    created the main function with only 1 valid user, the admin. Had difficulty implementing Gorilla Mux and GORM, planning to implement Gorilla Mux and either GORM or MongoDB for registration along with login page

--- main.go
+++ main.go
@@ -1,1 +1,26 @@
+package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			renderLoginPage(w, r)
+			return
+		}
+		username := r.FormValue("username")
+		password := r.FormValue("password")
+		if username == "admin" && password == "admin" {
+			fmt.Fprintln(w, "Welcome, admin!")
+			return
+		}
+		fmt.Fprintln(w, "Invalid username or password")
+	})
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
