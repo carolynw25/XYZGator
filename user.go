b00f1bb247b75db3132bafad89bb36437bc55850
Akhilesh-Makam <120730075+Akhilesh-Makam@users.noreply.github.com>
2023-02-25 22:06:07 -0500

    Add files via upload
    added two functions for testing

--- user.go
+++ user.go
@@ -33,10 +33,19 @@ func InitialMigration() {
 }
 
 func GetUsers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var users []User
+	DB.Find(&users)
+	json.NewEncoder(w).Encode(users)
 
 }
 
 func GetUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	var user User
+	DB.First(&user, params["id"])
+	json.NewEncoder(w).Encode(user)
 
 }
 
