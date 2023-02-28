796396061dd418acf8063ad0a5a692f453475b88
carolynw25 <100616643+carolynw25@users.noreply.github.com>
2023-02-28 03:44:53 -0500

    Login save info into object

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -53,36 +53,87 @@ import { UserService } from 'app/service/restapi.service';
 //     });
 //   }
 // }
+// @Component({
+//   selector: 'app-login',
+//   templateUrl: './login.component.html',
+//   styleUrls: ['./login.component.css']
+//   // //   template: `
+// // //   <form (submit)= "onSubmit()">
+// // //     <input type="text" [(ngModel)]="username" name="username">
+// // //     <input type="password" [(ngModel)]="password" name="password">
+// // //     <button type="submit">Login</button>
+// // // </form>
+// //   // `
+// //   templateUrl: './login.component.html',
+// //   styleUrls: ['./login.component.css'],
+// })
+// export class LoginComponent implements OnInit {
+
+//   username: string;
+//   password: string;
+
+//   constructor(private userService: UserService) {}
+
+//   onSubmit() {
+//     //call getUser function with the entered username as the ID
+//     this.userService.getUser(this.username).subscribe(user => {
+//     //Do something with the retrieved user data
+//       console.log(user);
+//     });
+//   }
+
+//   ngOnInit(): void {
+//   }
+
+//}
+
+//TAKE 2
+import {FormGroup, FormBuilder} from "@angular/forms"
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+interface IuserLogin{
+  username: string
+  password: string
+}
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
-  // //   template: `
-// //   <form (submit)= "onSubmit()">
-// //     <input type="text" [(ngModel)]="username" name="username">
-// //     <input type="password" [(ngModel)]="password" name="password">
-// //     <button type="submit">Login</button>
-// // </form>
-//   // `
-//   templateUrl: './login.component.html',
-//   styleUrls: ['./login.component.css'],
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  public username = ''
+  public password = ''
 
-  username: string;
-  password: string;
+  public userLogin: IuserLogin[] = [
+    {
+       username : 'fdaghnjmrsythdstyjhtrjydyutjhg',
+       password : 'gg',
+    }
+  ]
 
-  constructor(private userService: UserService) {}
+  public signupForm !: FormGroup;
+  constructor(private formBuilder : FormBuilder, private http : HttpClient, private router:Router) { }
 
-  onSubmit() {
-    //call getUser function with the entered username as the ID
-    this.userService.getUser(this.username).subscribe(user => {
-    //Do something with the retrieved user data
-      console.log(user);
-    });
+  checkUser(){
+    this.userLogin.push({
+      username: this.username,
+      password: this.password,
+    })
+    this.username = ''
+    this.password = ''
   }
 
+
+
+
   ngOnInit(): void {
+    
+    this.signupForm = this.formBuilder.group({
+      username:[''],
+      password:['']
+    })
+
   }
 
 }
