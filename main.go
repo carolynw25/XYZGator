742e37eeb2d995cad6c680fbc094611eee2db414
Akhilesh-Makam <120730075+Akhilesh-Makam@users.noreply.github.com>
2023-02-27 23:04:59 -0500

    Add files via upload
    changes to main for integration

--- main.go
+++ main.go
@@ -13,11 +13,11 @@ func initializeRouter() {
 	r := mux.NewRouter() //creates the router
 
 	//user methods
-	r.HandleFunc("/users", GetUsers).Methods("GET")
-	r.HandleFunc("/users/{id}", GetUser).Methods("GET")
-	r.HandleFunc("/users", CreateUser).Methods("POST")
-	r.HandleFunc("/users/{id}", UpdateUser).Methods("PUT")
-	r.HandleFunc("/users/{id}", DeleteUser).Methods("DELETE")
+	r.HandleFunc("/api/users", GetUsers).Methods("GET")
+	r.HandleFunc("/api/users/{id}", GetUser).Methods("GET")
+	r.HandleFunc("/api/users", CreateUser).Methods("POST")
+	r.HandleFunc("/api/users/{id}", UpdateUser).Methods("PUT")
+	r.HandleFunc("/api/users/{id}", DeleteUser).Methods("DELETE")
 
 	//launch server
 	//log.Fatal(http.ListenAndServe(":9000", r))
