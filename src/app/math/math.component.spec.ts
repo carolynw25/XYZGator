d9e316dac96031eddbb51c1103c9a3b8d8d6577a
carolynw25 <100616643+carolynw25@users.noreply.github.com>
2023-03-29 18:27:18 -0400

    minor fixes to singup

--- src/app/math/math.component.spec.ts
+++ src/app/math/math.component.spec.ts
@@ -133,9 +133,9 @@ describe('MathComponent', () => {
     expect(window.clearInterval).toHaveBeenCalled();
   });
 
-  // it('should navigate to "/notifications" when the return button is clicked', () => {
+  // it('should navigate to game page when the return button is clicked', () => {
   //   spyOn(component['router'], 'navigate');
-  //   const button = fixture.debugElement.query(By.css('.return-btn')).nativeElement;
+  //   const button = fixture.debugElement.query(By.css('.return')).nativeElement;
   //   button.click();
   //   expect(component['router'].navigate).toHaveBeenCalledWith(['/notifications']);  
   // });
