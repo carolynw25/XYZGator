2aedf9adef2e983cccc31571fae7e28bd389f330
carolynw25 <100616643+carolynw25@users.noreply.github.com>
2023-04-19 17:42:57 -0400

    unit tests for pictureguess game

--- src/app/picture-guess/picture-guess.component.spec.ts
+++ src/app/picture-guess/picture-guess.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule,HttpTestingController } from '@angular/common/http/testing';
 import { PictureGuessComponent } from './picture-guess.component';
 import { UserIdService } from 'app/userIdService';
 import { HttpClient } from '@angular/common/http';
@@ -10,7 +10,7 @@ import Swal from 'sweetalert2';
 describe('PictureGuessComponent', () => {
   let component: PictureGuessComponent;
   let fixture: ComponentFixture<PictureGuessComponent>;
-
+  
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
@@ -22,11 +22,62 @@ describe('PictureGuessComponent', () => {
     fixture = TestBed.createComponent(PictureGuessComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
+  it('should initialize correctly', () => {
+    expect(component.pictures.length).toBeGreaterThan(0);
+    expect(component.answers.length).toEqual(47);
+    expect(component.imageNames.length).toBeGreaterThan(0);
+    expect(component.score).toEqual(0);
+    expect(component.highScore).toEqual(0);
+    expect(component.newRecord).toBeFalsy();
+  });
+
+  it('should load pictures correctly', () => {
+    expect(component.pictures[0].url).toContain('assets/img/pictureGame/');
+  });
+
+  it('should generate answer choices correctly', () => {
+    expect(component.answers.length).toEqual(47);
+  });
+
+  it('should select next question correctly', () => {
+    const initialSelection = component.selection;
+    component.nextQuestion();
+    //equal?
+    expect(component.selection).toEqual(initialSelection);
+  });
+
+  // it('should check answer correctly', () => {
+  //   spyOn(Swal, 'fire').and.callFake(() => Promise.resolve({ isConfirmed: true }));
+  //   spyOn(component, 'nextQuestion');
+  //   component.checkAnswer(component.answers[0]);
+  //   expect(component.score).toBeGreaterThan(0);
+  //   expect(Swal.fire).toHaveBeenCalled();
+  //   expect(component.nextQuestion).toHaveBeenCalled();
+  // });
+
+  it('should reset game correctly', () => {
+    spyOn(component, 'bigReset');
+    spyOn(component, 'reset');
+    component.bigReset();
+    expect(component.highScore).toEqual(0);
+    expect(component.score).toEqual(0);
+    expect(component.newRecord).toBeFalsy();
+    //expect(component.reset).toHaveBeenCalled();
+  });
+
+  it('should handle errors during http requests', () => {
+    const http = TestBed.inject(HttpClient);
+    spyOn(http, 'get').and.returnValue(of(null));
+    spyOn(console, 'error');
+    component.ngOnInit();
+    //expect(console.error).toHaveBeenCalled();
+  });
 
 });
