9899f4d018d0d637ce104917cdae6f98366e665d
carolynw25 <100616643+carolynw25@users.noreply.github.com>
2023-04-18 21:45:59 -0400

    forget password set up, dashboard in the works

--- src/app/forget-password/forget-password.component.ts
+++ src/app/forget-password/forget-password.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { UserIdService } from 'app/userIdService';
+import {FormGroup, FormBuilder} from "@angular/forms";
+import { Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-forget-password',
@@ -7,9 +13,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForgetPasswordComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private formBuilder : FormBuilder, 
+    private http : HttpClient, 
+    private router:Router,
+    private userIdService: UserIdService) { }
+
+    public email = ''
 
   ngOnInit(): void {
+  }
+  onSubmit(): void {
+    const body = {email: this.email} ;
+    //const options = {headers: new HttpHeaders({'Content-Type':'application/json'}) };
+    const headers = {
+      'Content-Type': 'application/json'
+    };
+
+    this.http.post('http://127.0.0.1:8080/api/login', body, {headers}).subscribe
+      (response => {
+        //this.saveUserID(this.username, this.password)
+        // If the login is successful, redirect the user to the dashboard page
+        this.router.navigate(['/main']);
+        document.write("Welcome to the Web Page!");
+      },
+      error => {
+        // If the login is unsuccessful, display an error message
+        console.log(error);
+        //this.router.navigate(['/main']);
+      }
+    );
   }
 
 }
