0bb6f8cac425a15ff30b440cd05458d8e57d8af1
carolynw25 <100616643+carolynw25@users.noreply.github.com>
2023-04-19 18:38:42 -0400

    cleaned up user component

---
+++ src/app/pages/user/user.component.spec.ts
@@ -0,0 +1,35 @@
+// import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
+// import { ReactiveFormsModule, FormControlDirective } from '@angular/forms';
+// import { RouterTestingModule } from '@angular/router/testing';
+// import { UserComponent } from './user.component';
+// import { UserIdService } from 'app/userIdService';
+// import { Observable, of } from 'rxjs';
+// import { NgControl } from '@angular/forms';
+
+
+// describe('UserComponent', () => {
+//   let component: UserComponent;
+//   let fixture: ComponentFixture<UserComponent>;
+
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       imports: [ReactiveFormsModule, RouterTestingModule, HttpClientTestingModule],
+//       declarations: [UserComponent],
+//       providers: [UserIdService, FormControlDirective],
+//     }).compileComponents();
+//   });
+
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(UserComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+
+
+
+// });
