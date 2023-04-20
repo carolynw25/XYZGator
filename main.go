859fec83bea5bf1f504cb7f2aada07c28c420413
Akhilesh-Makam <120730075+Akhilesh-Makam@users.noreply.github.com>
2023-04-19 21:06:03 -0400

    final versions of back end

--- main.go
+++ main.go
@@ -6,9 +6,6 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/rs/cors"
-	//"gorm.io/gorm"
-	//"gorm.io/driver/sqlite"
-	//"golang.org/x/crypto/bcrypt"
 )
 
 func initializeRouter() {
@@ -17,17 +14,16 @@ func initializeRouter() {
 	//user methods
 	r.HandleFunc("/api/users", GetUsers).Methods("GET")
 	r.HandleFunc("/api/users/{id}", GetUser).Methods("GET")
-	//r.HandleFunc("/api/users", CreateUser).Methods("POST")
-	r.HandleFunc("/api/users/{id}", UpdateUser).Methods("PUT")
-	r.HandleFunc("/api/users/{id}", DeleteUser).Methods("DELETE")
+	// r.HandleFunc("/api/users/{id}", DeleteUser).Methods("DELETE")
 	r.HandleFunc("/api/login", login).Methods("POST")
 	r.HandleFunc("/api/signUp", signUp).Methods("POST")
 	r.HandleFunc("/api/getID", getID).Methods("POST")
+	
 	r.HandleFunc("/api/users/{id}/name", UpdateUsername).Methods("PUT")
     r.HandleFunc("/api/users/{id}/pass", UpdatePassword).Methods("PUT")
 	r.HandleFunc("/api/users/{id}/first", UpdateFirstName).Methods("PUT")
 	r.HandleFunc("/api/users/{id}/last", UpdateLastName).Methods("PUT")
-	r.HandleFunc("/api/users/{id}/email}", UpdateEmail).Methods("PUT")
+	r.HandleFunc("/api/users/{id}/email", UpdateEmail).Methods("PUT")
 
 	r.HandleFunc("/api/users/{id}/matchscore", GetMatchScore).Methods("GET")
 	r.HandleFunc("/api/users/{id}/mathscore", GetMathScore).Methods("GET")
@@ -43,7 +39,6 @@ func initializeRouter() {
 	
 	
     
-	//r.HandleFunc("/api/test", login).Methods("POST")
 
 	//adds CORS middleware
 	c := cors.New(cors.Options{
@@ -61,10 +56,7 @@ func initializeRouter() {
 		})
 	}(handler)
 
-	//launch server
-	//log.Fatal(http.ListenAndServe(":9000", r))
 	log.Fatal(http.ListenAndServe("localhost:8080", handler))
-	//log.Fatal(http.ListenAndServe("128.227.1.31:61478", r))
 }
 
 
--- main_test.go
+++ main_test.go
@@ -3,20 +3,43 @@ package main
 import (
 	"bytes"
 	"encoding/json"
-	//"fmt"
-	//"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"strings"
-	//"github.com/rs/cors"
-	//"golang.org/x/crypto/bcrypt"
+	"golang.org/x/crypto/bcrypt"
 	"testing"
-	//"github.com/stretchr/testify/assert"
 	"github.com/gorilla/mux"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-	//"gorm.io/driver/sqlite"
 )
+
+func TestGetUser(t *testing.T) {
+    // Initialize a new router instance and register the GetUser function as a handler for the GET request
+    r := mux.NewRouter()
+    r.HandleFunc("/users/{id}", GetUser).Methods("GET")
+
+    // Create a new instance of httptest.ResponseRecorder to record the response
+    w := httptest.NewRecorder()
+
+    // Create a new request to the /users/{id} endpoint with an id of 1
+    req, err := http.NewRequest("GET", "/users/1", nil)
+    if err != nil {
+        t.Fatal(err)
+    }
+
+    // Initialize the database connection
+    DB, _ = gorm.Open(mysql.Open(DNS), &gorm.Config{})
+
+    // Call the GetUser function with the response recorder and request objects
+   GetUser(w, req)
+
+    // Assert that the response status code is 200 OK
+    if status := w.Code; status != http.StatusOK {
+        t.Errorf("Handler returned wrong status code: got %v want %v",
+            status, http.StatusOK)
+    }
+}
+
 
 func TestSignUp(t *testing.T) {
 	// Initialize a new router instance and register the SignUp function as a handler for the POST request
@@ -62,7 +85,7 @@ func TestLogin(t *testing.T) {
 	w := httptest.NewRecorder()
 
 	// Create a new request to the /login endpoint with a username and password in the request body
-	loginData := []byte(`{"username": "vishalj05", "password": "wack"}`) //make sure this user is in database
+	loginData := []byte(`{"username": "johnsmith", "password": "password123"}`) //make sure this user is in database
 	req, err := http.NewRequest("POST", "/login", bytes.NewBuffer(loginData))
 	if err != nil {
 		t.Fatal(err)
@@ -89,14 +112,14 @@ func TestLogin(t *testing.T) {
 func TestGetID(t *testing.T) {
 	// Initialize a new router instance and register the GetID function as a handler for the GET request
 	r := mux.NewRouter()
-	r.HandleFunc("/getid", getID).Methods("GET")
+	r.HandleFunc("/getID", getID).Methods("GET")
 
 	// Create a new instance of httptest.ResponseRecorder to record the response
 	w := httptest.NewRecorder()
 
 	// Create a new request to the /login endpoint with a username and password in the request body
-	loginData := []byte(`{"username": "vishalj05", "password": "wack"}`)
-	req, err := http.NewRequest("GET", "/getid", bytes.NewBuffer(loginData))
+	loginData := []byte(`{"username": "johnsmith", "password": "password123"}`)
+	req, err := http.NewRequest("GET", "/getID", bytes.NewBuffer(loginData))
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -155,12 +178,56 @@ func TestUpdateUsername(t *testing.T) {
 	}
 }
 
-//TEST UPDATE PASSWORD STILL NEEDED
+func TestUpdatePassword(t *testing.T) {
+	// Initialize a new router instance and register the UpdatePassword function as a handler for the PUT request
+	r := mux.NewRouter()
+	r.HandleFunc("/users/{id}/pass", UpdatePassword).Methods("PUT")
+
+	// Create a new instance of httptest.ResponseRecorder to record the response
+	w := httptest.NewRecorder()
+
+	// Create a new request to the /users/{id}/pass endpoint with an id of 1 and a request body containing updated username data
+	updateUser := User{
+		Password: "wack3",
+	}
+	updateUserJSON, _ := json.Marshal(updateUser)
+	req, err := http.NewRequest("PUT", "/users/1/pass", bytes.NewReader(updateUserJSON))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Call the UpdatePassword function with the response recorder and request objects
+	DB, _ = gorm.Open(mysql.Open(DNS), &gorm.Config{})
+	UpdatePassword(w, req)
+
+	// Assert that the response status code is 200 OK
+	if status := w.Code; status != http.StatusOK {
+		t.Errorf("Handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	// Retrieve the updated user from the database
+	var updatedUser User
+	DB.First(&updatedUser, 1)
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(updateUser.Password), bcrypt.DefaultCost)
+    if err != nil {
+        w.WriteHeader(http.StatusInternalServerError)
+        json.NewEncoder(w).Encode(map[string]string{"error": "Failed to hash password"})
+        return
+    }
+
+	// Assert that the updated user data in the database matches the request body data
+	if bytes.Equal([]byte(updatedUser.Password), hashedPassword) {
+		t.Errorf("Handler did not update username data correctly: got %v want %v",
+			updatedUser.Password, hashedPassword)
+	}
+}
 
 func TestUpdateFirstname(t *testing.T) {
-	// Initialize a new router instance and register the UpdateUsername function as a handler for the PUT request
+	// Initialize a new router instance and register the UpdateFirstName function as a handler for the PUT request
 	r := mux.NewRouter()
-	r.HandleFunc("/users/{id}/first", UpdateUsername).Methods("PUT")
+	r.HandleFunc("/users/{id}/first", UpdateFirstName).Methods("PUT")
 
 	// Create a new instance of httptest.ResponseRecorder to record the response
 	w := httptest.NewRecorder()
@@ -198,14 +265,14 @@ func TestUpdateFirstname(t *testing.T) {
 
 
 func TestUpdateLastname(t *testing.T) {
-	// Initialize a new router instance and register the UpdateUsername function as a handler for the PUT request
+	// Initialize a new router instance and register the UpdateLastName function as a handler for the PUT request
 	r := mux.NewRouter()
-	r.HandleFunc("/users/{id}/last", UpdateUsername).Methods("PUT")
+	r.HandleFunc("/users/{id}/last", UpdateLastName).Methods("PUT")
 
 	// Create a new instance of httptest.ResponseRecorder to record the response
 	w := httptest.NewRecorder()
 
-	// Create a new request to the /users/{id}/username endpoint with an id of 1 and a request body containing updated username data
+	// Create a new request to the /users/{id}/last endpoint with an id of 1 and a request body containing updated username data
 	updateUser := User{
 		LastName: "PATEL",
 	}
@@ -237,14 +304,14 @@ func TestUpdateLastname(t *testing.T) {
 }
 
 func TestUpdateEmail(t *testing.T) {
-	// Initialize a new router instance and register the UpdateFirstName function as a handler for the PUT request
+	// Initialize a new router instance and register the UpdateEmail function as a handler for the PUT request
 	r := mux.NewRouter()
 	r.HandleFunc("/users/{id}/email", UpdateEmail).Methods("PUT")
 
 	// Create a new instance of httptest.ResponseRecorder to record the response
 	w := httptest.NewRecorder()
 
-	// Create a new request to the /users/{id}/firstname endpoint with an id of 1 and a request body containing updated user data
+	// Create a new request to the /users/{id}/email endpoint with an id of 1 and a request body containing updated user data
 	updateUser := User{
 		Email: "randomemail@something.com",
 	}
@@ -254,7 +321,7 @@ func TestUpdateEmail(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	// Call the UpdateFirstName function with the response recorder and request objects
+	// Call the UpdateEmail function with the response recorder and request objects
 	DB, _ = gorm.Open(mysql.Open(DNS), &gorm.Config{})
 	UpdateEmail(w, req)
 
@@ -305,7 +372,7 @@ func TestGetMatchScore(t *testing.T) {
 }
 
 func TestSetMatchScore(t *testing.T) {
-    // Initialize a new router instance and register the SetMathScore function as a handler for the PUT request
+    // Initialize a new router instance and register the SetMatchScore function as a handler for the PUT request
     r := mux.NewRouter()
     r.HandleFunc("/users/{id}/match/{target}/score", setMatchScore).Methods("PUT")
 
@@ -322,7 +389,7 @@ func TestSetMatchScore(t *testing.T) {
     // Initialize the database connection
     DB, _ = gorm.Open(mysql.Open(DNS), &gorm.Config{})
 
-    // Call the SetMathScore function with the response recorder and request objects
+    // Call the SetMatchScore function with the response recorder and request objects
     setMatchScore(w, req)
 
     // Assert that the response status code is 200 OK
@@ -335,14 +402,14 @@ func TestSetMatchScore(t *testing.T) {
 //MATH TESTS
 
 func TestGetMathScore(t *testing.T) {
-    // Initialize a new router instance and register the GetMatchScore function as a handler for the GET request
+    // Initialize a new router instance and register the GetMathScore function as a handler for the GET request
     r := mux.NewRouter()
     r.HandleFunc("/users/{id}/math/{target}", GetMathScore).Methods("GET")
 
     // Create a new instance of httptest.ResponseRecorder to record the response
     w := httptest.NewRecorder()
 
-    // Create a new request to the /users/{id}/match/{target} endpoint with an id of 1 and a target of 2
+    // Create a new request to the /users/{id}/math/{target} endpoint with an id of 1 and a target of 2
     req, err := http.NewRequest("GET", "/users/1/math/2", nil)
     if err != nil {
         t.Fatal(err)
@@ -351,7 +418,7 @@ func TestGetMathScore(t *testing.T) {
     // Initialize the database connection
     DB, _ = gorm.Open(mysql.Open(DNS), &gorm.Config{})
 
-    // Call the GetMatchScore function with the response recorder and request objects
+    // Call the GetMathScore function with the response recorder and request objects
     GetMathScore(w, req)
 
     // Assert that the response status code is 200 OK
@@ -364,14 +431,14 @@ func TestGetMathScore(t *testing.T) {
 func TestSetMathScore(t *testing.T) {
     // Initialize a new router instance and register the SetMathScore function as a handler for the PUT request
     r := mux.NewRouter()
-    r.HandleFunc("/users/{id}/match/{target}/score", setMathScore).Methods("PUT")
+    r.HandleFunc("/users/{id}/math/{target}/score", setMathScore).Methods("PUT")
 
     // Create a new instance of httptest.ResponseRecorder to record the response
     w := httptest.NewRecorder()
 
-    // Create a new request to the /users/{id}/match/{target}/score endpoint with an id of 1 and a target of 2 and a score of 75
+    // Create a new request to the /users/{id}/math/{target}/score endpoint with an id of 1 and a target of 2 and a score of 75
     body := bytes.NewBuffer([]byte(`{"score":75}`))
-    req, err := http.NewRequest("PUT", "/users/1/match/2/score", body)
+    req, err := http.NewRequest("PUT", "/users/1/math/2/score", body)
     if err != nil {
         t.Fatal(err)
     }
@@ -391,15 +458,15 @@ func TestSetMathScore(t *testing.T) {
 
 //WORD SCORE
 func TestGetWordScore(t *testing.T) {
-    // Initialize a new router instance and register the GetMatchScore function as a handler for the GET request
+    // Initialize a new router instance and register the GetWordScore function as a handler for the GET request
     r := mux.NewRouter()
-    r.HandleFunc("/users/{id}/word/{target}", GetMathScore).Methods("GET")
+    r.HandleFunc("/users/{id}/word/{target}", GetWordScore).Methods("GET")
 
     // Create a new instance of httptest.ResponseRecorder to record the response
     w := httptest.NewRecorder()
 
-    // Create a new request to the /users/{id}/match/{target} endpoint with an id of 1 and a target of 2
-    req, err := http.NewRequest("GET", "/users/1/math/2", nil)
+    // Create a new request to the /users/{id}/word/{target} endpoint with an id of 1 and a target of 2
+    req, err := http.NewRequest("GET", "/users/1/word/2", nil)
     if err != nil {
         t.Fatal(err)
     }
@@ -407,7 +474,7 @@ func TestGetWordScore(t *testing.T) {
     // Initialize the database connection
     DB, _ = gorm.Open(mysql.Open(DNS), &gorm.Config{})
 
-    // Call the GetMatchScore function with the response recorder and request objects
+    // Call the GetWordScore function with the response recorder and request objects
     GetWordScore(w, req)
 
     // Assert that the response status code is 200 OK
@@ -418,16 +485,16 @@ func TestGetWordScore(t *testing.T) {
 }
 
 func TestSetWordScore(t *testing.T) {
-    // Initialize a new router instance and register the SetMathScore function as a handler for the PUT request
+    // Initialize a new router instance and register the SetWordScore function as a handler for the PUT request
     r := mux.NewRouter()
-    r.HandleFunc("/users/{id}/word/{target}/score", setMathScore).Methods("PUT")
+    r.HandleFunc("/users/{id}/word/{target}/score", setWordScore).Methods("PUT")
 
     // Create a new instance of httptest.ResponseRecorder to record the response
     w := httptest.NewRecorder()
 
-    // Create a new request to the /users/{id}/match/{target}/score endpoint with an id of 1 and a target of 2 and a score of 75
+    // Create a new request to the /users/{id}/word/{target}/score endpoint with an id of 1 and a target of 2 and a score of 75
     body := bytes.NewBuffer([]byte(`{"score":75}`))
-    req, err := http.NewRequest("PUT", "/users/1/match/2/score", body)
+    req, err := http.NewRequest("PUT", "/users/1/word/2/score", body)
     if err != nil {
         t.Fatal(err)
     }
@@ -435,7 +502,7 @@ func TestSetWordScore(t *testing.T) {
     // Initialize the database connection
     DB, _ = gorm.Open(mysql.Open(DNS), &gorm.Config{})
 
-    // Call the SetMathScore function with the response recorder and request objects
+    // Call the SetWordScore function with the response recorder and request objects
     setWordScore(w, req)
 
     // Assert that the response status code is 200 OK
@@ -448,15 +515,15 @@ func TestSetWordScore(t *testing.T) {
 //ANIMAL TESTS
 
 func TestGetAnimalScore(t *testing.T) {
-    // Initialize a new router instance and register the GetMatchScore function as a handler for the GET request
+    // Initialize a new router instance and register the GetAnimalScore function as a handler for the GET request
     r := mux.NewRouter()
-    r.HandleFunc("/users/{id}/animal/{target}", GetMathScore).Methods("GET")
+    r.HandleFunc("/users/{id}/animal/{target}", GetAnimalScore).Methods("GET")
 
     // Create a new instance of httptest.ResponseRecorder to record the response
     w := httptest.NewRecorder()
 
-    // Create a new request to the /users/{id}/match/{target} endpoint with an id of 1 and a target of 2
-    req, err := http.NewRequest("GET", "/users/1/math/2", nil)
+    // Create a new request to the /users/{id}/animal/{target} endpoint with an id of 1 and a target of 2
+    req, err := http.NewRequest("GET", "/users/1/animal/2", nil)
     if err != nil {
         t.Fatal(err)
     }
@@ -464,7 +531,7 @@ func TestGetAnimalScore(t *testing.T) {
     // Initialize the database connection
     DB, _ = gorm.Open(mysql.Open(DNS), &gorm.Config{})
 
-    // Call the GetMatchScore function with the response recorder and request objects
+    // Call the GetAnimalScore function with the response recorder and request objects
     GetAnimalScore(w, req)
 
     // Assert that the response status code is 200 OK
@@ -475,16 +542,16 @@ func TestGetAnimalScore(t *testing.T) {
 }
 
 func TestSetAnimalScore(t *testing.T) {
-    // Initialize a new router instance and register the SetMathScore function as a handler for the PUT request
+    // Initialize a new router instance and register the SetAnimalScore function as a handler for the PUT request
     r := mux.NewRouter()
-    r.HandleFunc("/users/{id}/match/{target}/score", setMathScore).Methods("PUT")
+    r.HandleFunc("/users/{id}/animal/{target}/score", setAnimalScore).Methods("PUT")
 
     // Create a new instance of httptest.ResponseRecorder to record the response
     w := httptest.NewRecorder()
 
-    // Create a new request to the /users/{id}/match/{target}/score endpoint with an id of 1 and a target of 2 and a score of 75
+    // Create a new request to the /users/{id}/animal/{target}/score endpoint with an id of 1 and a target of 2 and a score of 75
     body := bytes.NewBuffer([]byte(`{"score":75}`))
-    req, err := http.NewRequest("PUT", "/users/1/match/2/score", body)
+    req, err := http.NewRequest("PUT", "/users/1/animal/2/score", body)
     if err != nil {
         t.Fatal(err)
     }
@@ -492,8 +559,38 @@ func TestSetAnimalScore(t *testing.T) {
     // Initialize the database connection
     DB, _ = gorm.Open(mysql.Open(DNS), &gorm.Config{})
 
-    // Call the SetMathScore function with the response recorder and request objects
+    // Call the SetAnimalScore function with the response recorder and request objects
     setAnimalScore(w, req)
+
+    // Assert that the response status code is 200 OK
+    if status := w.Code; status != http.StatusOK {
+        t.Errorf("Handler returned wrong status code: got %v want %v",
+            status, http.StatusOK)
+    }
+}
+
+func TestForgotPassword(t *testing.T) {
+    // Initialize a new router instance and register the ForgotPassword function as a handler for the POST request
+    r := mux.NewRouter()
+    r.HandleFunc("/forgotPassword", ForgotPassword).Methods("POST")
+
+    // Create a new instance of httptest.ResponseRecorder to record the response
+    w := httptest.NewRecorder()
+
+    // Create a new request to the /forgotPassword endpoint with a request body containing user data
+    requestBody := []byte(`{
+        "email": "viJap@gmail.com",
+        "favoriteAnimal": "DOLPHIN",
+        "password": "wack3"
+    }`)
+    req, err := http.NewRequest("POST", "/forgotPassword", bytes.NewReader(requestBody))
+    if err != nil {
+        t.Fatal(err)
+    }
+
+    // Call the ForgotPassword function with the response recorder and request objects
+    DB, _ = gorm.Open(mysql.Open(DNS), &gorm.Config{})
+    ForgotPassword(w, req)
 
     // Assert that the response status code is 200 OK
     if status := w.Code; status != http.StatusOK {
